19adeb0f070fdf413a9e40a33ccac850b6333698
redouane <redouaneboukamir@gmail.com>
2019-03-06 19:13:30 +0100

    save repository

--- src/Controller/propertyController.php
+++ src/Controller/propertyController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+// Relation avec le contenu des entité et variable de la db
 // use Symfony\Component\Routing\Annotation\Route;
 
 class propertyController extends AbstractController
@@ -14,8 +16,36 @@ class propertyController extends AbstractController
     //  * @return Response
     //  */
 
+    // On réucpére la classe repository et en fait un constructeur
+    public function __construct(PropertyRepositoy $repository)
+    {
+        $this->repository = $repository;
+    }
 
     public function index():Response{
+        // Create de property et remplissage de setter dans la variable
+        // $property = new Property();
+        // $property->setTitle('Mon premier titre')
+        //     ->setPrice(2000)
+        //     ->setRooms(4)
+        //     ->setBedrooms(3)
+        //     ->setDescription('Une petite description')
+        //     ->setSurface(60)
+        //     ->setFloor(4)
+        //     ->setHeat(1)
+        //     ->setCity('charleroi')
+        //     ->setAddress('rue test')
+        //     ->setPostalCode('6000');
+        // // Créer une entité manager pour envoyer a la DB
+        // $em = $this->getDoctrine()->getManager();
+        // // Persiste mon entité
+        // $em->persist($property);
+        // // Envois vers la DB
+        // $em->flush();
+
+        // Injection du repository Property
+        $repository = $this->getDoctrine()->getRepository(Property::class);
+        dump($repository);
         return $this->render('property/index.html.twig',[
             'current_menu' => 'properties'
         ]);
--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -9,6 +9,11 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Property
 {
+    const HEAT = [
+        0 => "eletrique",
+        1 => "gaz",
+        2 => "test"
+    ];
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -25,6 +30,66 @@ class Property
      * @ORM\Column(type="text", nullable=true)
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $surface;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $rooms;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bedrooms;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $floor;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $heat;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $postal_code;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default": false})
+     */
+    private $sold=false;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $create_at;
+
+    public function __construct(){
+        $this->create_at = new \DateTime();
+        
+    }
 
     public function getId(): ?int
     {
@@ -51,6 +116,138 @@ class Property
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSurface(): ?int
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(int $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getRooms(): ?int
+    {
+        return $this->rooms;
+    }
+
+    public function setRooms(int $rooms): self
+    {
+        $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    public function getBedrooms(): ?int
+    {
+        return $this->bedrooms;
+    }
+
+    public function setBedrooms(int $bedrooms): self
+    {
+        $this->bedrooms = $bedrooms;
+
+        return $this;
+    }
+
+    public function getFloor(): ?int
+    {
+        return $this->floor;
+    }
+
+    public function setFloor(int $floor): self
+    {
+        $this->floor = $floor;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getHeat(): ?int
+    {
+        return $this->heat;
+    }
+
+    public function setHeat(int $heat): self
+    {
+        $this->heat = $heat;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(string $postal_code): self
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getSold(): ?bool
+    {
+        return $this->sold;
+    }
+
+    public function setSold(bool $sold): self
+    {
+        $this->sold = $sold;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->create_at;
+    }
+
+    public function setCreateAt(\DateTimeInterface $create_at): self
+    {
+        $this->create_at = $create_at;
 
         return $this;
     }
