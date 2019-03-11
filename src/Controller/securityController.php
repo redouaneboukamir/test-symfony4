5ae05dca75c04137b3dd8a6133044973629c3b15
redouane <redouaneboukamir@gmail.com>
2019-03-11 10:47:28 +0100

    Debut 6emePartie

--- src/Controller/securityController.php
+++ src/Controller/securityController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class securityController extends AbstractController
 {
-    /**
-     *@Route('/login', name='login')
-     */
-    public function login(){
-        return $this->render('security/login.html.twig');
+
+    public function login(AuthenticationUtils $authenticationUtils){
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+
     }
 }
